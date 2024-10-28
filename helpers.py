c9df1c54b145b6b43f50bed1453fa82f410b0ed4
Paul Slepko <pslepko.ps@gmail.com>
2024-10-28 22:11:35 +0100

    lots of changes

--- helpers.py
+++ helpers.py
@@ -1,10 +1,48 @@
+
+
+import random
+import math
+
+mines = []
+
 def reset():
     print("Reset")
 
 
 
-
+def check_borders(row, column):
+    global mines
+    bordered_mines = 0
+    
+    
+    
+    return bordered_mines
 
 
 def check_mines(row, column):
-    print(str(row)+str(column))
+    global mines
+    
+    index = row*10+column
+    
+    print(index)
+    if mines.__contains__(index):
+        return True
+    
+    return check_borders(row, column)
+    
+    
+    
+    
+def set_mines(mine_count):
+    global mines
+    size = 100
+    for i in range(mine_count):
+        number = math.floor(size*random.random())  #create a random number 
+        if mines.__contains__(number) :             #check if number already created
+            i -=1                                   #if yes decrease i to generate a new random number
+        else : mines.append(number)                 #else add number to minelist
+        
+    mines.sort()
+    
+        
+    print(mines)
--- main.py
+++ main.py
@@ -1,30 +1,36 @@
-import random
-from tkinter import *
+
+import tkinter as tk
 import helpers
 
 
 
 
 # Ein Fenster erstellen
-window = Tk()
+window = tk.Tk()
 # Den Fenstertitle erstellen
 window.title("Minesweeper")
 
 x = 10
 y = 10
+mine_count = 10
 
 
 
-reset_button  = Button(window, text="Reset", command=helpers.reset())
-exit = Button(window, text="Close", command=window.quit)
+reset_button  = tk.Button(window, text="Reset", command=helpers.reset())
+exit = tk.Button(window, text="Close", command=window.quit)
 
 mine = [[dim for dim in range(y)] for dim in range(x)]  # x*y dimensional list of lists
-mine_label = Label()
+
+helpers.set_mines(mine_count)
 
 for i in range(x):
     for j in range(y):
         # arrange the Buttons in a grid in x*y dimension and call check_mines on click with row and column as parameter
-        mine_button = Button(window, height=1, width=2, command=lambda row=i, column=j: helpers.check_mines(row, column)).grid(row=i+1, column=j+1, padx=0, pady=0)   
+        mine_button = tk.Button(window, height=1, width=2, text=" ", command=lambda row=i, column=j: 
+            helpers.check_mines(row, column)).grid(row=i+1, column=j+1, padx=0, pady=0)
+        
+        
+
 
 # print(mine) # test
 
