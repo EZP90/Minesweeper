3f7bf9f717d8614d35bec962a337c1a8ff98778a
Paul Slepko <pslepko.ps@gmail.com>
2024-11-13 18:15:40 +0100

    updates

--- helpers.py
+++ helpers.py
@@ -1,5 +1,5 @@
 
-
+import tkinter as tk
 import random
 import math
 
@@ -14,21 +14,37 @@ def check_borders(row, column):
     global mines
     bordered_mines = 0
     
+    for i in [-1, 0 , 1]:
+        for j in [-1, 0, 1]:
+            index = (row+i)*10+(column+j)
+            if mines.__contains__(index):
+                #print(index)
+                bordered_mines += 1
     
     
     return bordered_mines
 
 
-def check_mines(row, column):
+def check_mines(row, column, button):
     global mines
+    
     
     index = row*10+column
     
     print(index)
     if mines.__contains__(index):
-        return True
-    
-    return check_borders(row, column)
+        try:
+            button[row][column].config(text="X")
+        except AttributeError:
+            print("Error: The object passed is not a tkinter Button.")
+            print(f"Received type: {type(button)}")
+    else:
+        amount = check_borders(row, column)
+        try:
+            button[row][column].config(text=amount)
+        except AttributeError:
+            print("Error: The object passed is not a tkinter Button.")
+            print(f"Received type: {type(button)}")
     
     
     
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import tkinter as tk
 import helpers
+from functools import partial
 
 
 
@@ -10,25 +11,31 @@ window = tk.Tk()
 # Den Fenstertitle erstellen
 window.title("Minesweeper")
 
+topframe = tk.Frame(window, bg="grey", padx=10, pady=10)
+topframe.pack(side="top")
+mineframe = tk.Frame(window, bg="grey", padx=0, pady=0)
+mineframe.pack(fill="both", expand=True)
+
 x = 10
 y = 10
 mine_count = 10
+mine_button = [[dim for dim in range(y)] for dim in range(x)]  # x*y dimensional list of lists
 
 
 
-reset_button  = tk.Button(window, text="Reset", command=helpers.reset())
-exit = tk.Button(window, text="Close", command=window.quit)
+reset_button  = tk.Button(topframe, text="Reset", command=partial(helpers.reset)).grid(row=0, column=0)
+exit = tk.Button(topframe, text="Close", command=window.quit).grid(row=0, column=1)
 
-mine = [[dim for dim in range(y)] for dim in range(x)]  # x*y dimensional list of lists
+
 
 helpers.set_mines(mine_count)
 
 for i in range(x):
     for j in range(y):
+        #print(i,j)
         # arrange the Buttons in a grid in x*y dimension and call check_mines on click with row and column as parameter
-        mine_button = tk.Button(window, height=1, width=2, text=" ", command=lambda row=i, column=j: 
-            helpers.check_mines(row, column)).grid(row=i+1, column=j+1, padx=0, pady=0)
-        
+        mine_button[i][j] = tk.Button(mineframe, height=1, width=2, text=" ", command=partial(helpers.check_mines,i,j,mine_button))
+        mine_button[i][j].grid(row=i+1, column=j+1, padx=0, pady=0)
         
 
 
